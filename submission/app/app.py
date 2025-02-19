e6dbd73c83a37904b9599782713dd241b180a1e5
Tyler Beringer <99tylerberinger@gmail.com>
2025-02-19 04:28:01 -0500

    Added dashboard.js (with a table)

--- submission/app/app.py
+++ submission/app/app.py
@@ -1,54 +1,30 @@
-#Import the dependencies 
-
+from flask import Flask, jsonify, render_template
 import pandas as pd
-from flask import Flask, jsonify, render_template, redirect, request
+import sqlite3
 from sqlHelper import SQLHelper
 
-######################## Flask Setup #####################################
-
 app = Flask(__name__)
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0 # remove caching
-
-# SQL Helper
 sqlHelper = SQLHelper()
 
-######################## Flask Routes #####################################
-
-@app.route("/")
-def home():
-    return render_template("pages/home.html")
-
-@app.route("/dashboard1")
-def dashboard1():
-    return render_template("dashboard1.html")
-
-@app.route("/dashboard2")
-def dashboard2():
-    return render_template("dashboard2.html")
-
-@app.route("/map")
-def map():
-    return render_template("map.html")
-
-@app.route("/about")
-def about():
-    return render_template("pages/about.html")
-
-@app.route("/sources")
-def sources():
-    return render_template("pages/sources.html")
-
-#######################################################################
-
+# Flask API
 @app.route("/api/v1.0/table")
 def table():
-    # Execute Query
-    df = sqlHelper.queryTableData()
+    with sqlite3.connect("../database/data/database.db") as connection:
+        cursor = connection.cursor()
 
-    # Turn DataFrame into List of Dictionary
-    data = df.to_dict(orient="records")
-    return jsonify(data)
+        cursor.execute(
+                        """
+                        SELECT year, state, SUM(fatalities) AS num_of_fatalities, SUM(injuries) AS num_of_injuries, COUNT(*) AS num_of_tornadoes
+                        FROM tornado
+                        GROUP BY year, state;
+                        """
+        )
 
+        columns = [description[0] for description in cursor.description]
+        rows = cursor.fetchall()
+
+        data = [dict(zip(columns,row)) for row in rows]
+        return jsonify(data)
 
 @app.route("/api/v1.0/linechart_data")
 def linechart_data():
@@ -109,22 +85,35 @@ def map_data():
     data = df.to_dict(orient="records")
     return jsonify(data)
 
-#############################################################
+# Flask pages
+@app.route("/")
+def home():
+    return render_template("pages/home.html")
 
-# ELIMINATE CACHING
-@app.after_request
-def add_header(r):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    r.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    return r
+@app.route("/dashboard")
+def dashboard():
+    return render_template("pages/dashboard.html")
 
-#############################################################
+@app.route("/dashboard1")
+def dashboard1():
+    return render_template("dashboard1.html")
 
-if __name__ == '__main__':
+@app.route("/dashboard2")
+def dashboard2():
+    return render_template("dashboard2.html")
+
+@app.route("/map")
+def map():
+    return render_template("map.html")
+
+@app.route("/about")
+def about():
+    return render_template("pages/about.html")
+
+@app.route("/sources")
+def sources():
+    return render_template("pages/sources.html")
+
+# main
+if __name__ == "__main__":
     app.run(debug=True)
